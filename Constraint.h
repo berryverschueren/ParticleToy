ec64ed6c9fe8bf7876de6db65db445b4653f1202
carinacornet <45971826+carinacornet@users.noreply.github.com>
2019-05-18 10:48:28 +0200

    General Constraint Class

---
+++ Constraint.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Particle.h"
+#include <vector>
+
+class Constraint {
+public:
+    Constraint(Particle *p1, Particle *p2, double dist);
+    float constraint_value();
+    float constraint_derivative_value();
+    std::vector<Vec2f> jacobian_value();
+    std::vector<Vec2f> jacobian_derivative_value();
+    void draw();
+
+private:
+
+    Particle * const m_p1;
+    Particle * const m_p2;
+    double const m_dist;
+
+};
